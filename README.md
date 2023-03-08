4f2cb4652e625405f4af8c5b8b87ea29f4374fa8
Taaaaab <owen.whitsett@gmail.com>
2023-03-08 12:35:51 -0500

    Update Readme.md and apply Prettier to index.js

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # blog-api
-Blog created using Express and MongoDB. Project to practice using an API only backend to obtain posts and comments.
+
+==========
+Blog created using Express and MongoDB (with Mongoose). Project to practice using an API only backend to create, read, update and delete blog posts and comments (saved using Mongoose schema to MongoDB Atlas). Homepage with script to fetch and display blog posts and comments. Bootstrap used for CSS styling of cards.
+
+API to fetch posts (on localhost port 3000):
+http://localhost:3000/api/posts
+
+## Installation
+
+Run blog-api by cloning repository, installing dependencies and using npm to start (using nodemon):
+
+    npm install
+    npm run start
+
+## License
+
+The project is licensed under the ISC license.
 
 ![Alt text](https://github.com/Taaaaab/personal-portfolio/blob/main/photos/blog.png?raw=true "Screenshot")
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
-import async from 'async';
-import routes from './routes';
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import async from "async";
+import routes from "./routes";
 const compression = require("compression");
 // Import the mongoose module
-const mongoose = require('mongoose');
-var path = require('path');
+const mongoose = require("mongoose");
+var path = require("path");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 // * Application-Level Middleware * //
 
@@ -30,24 +30,24 @@ app.use(express.urlencoded({ extended: true }));
 // Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
 // Included because it removes preparatory warnings for Mongoose 7.
 // See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 // Define the database URL to connect to.
 const mongoDB = process.env.MONGODB_URL;
 
-// Wait for database to connect, logging an error if there is a problem 
-main().catch(err => console.log(err));
+// Wait for database to connect, logging an error if there is a problem
+main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
 
 // * Routes * //
 
-app.use('/', routes.posts);
-app.use('/posts', routes.posts);
-app.use('/api', routes.api);
+app.use("/", routes.posts);
+app.use("/posts", routes.posts);
+app.use("/api", routes.api);
 
 // * Start * //
 
 app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
+  console.log(`Example app listening on port ${process.env.PORT}!`)
 );
